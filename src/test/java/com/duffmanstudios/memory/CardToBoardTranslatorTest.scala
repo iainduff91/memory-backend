0a1d32760379d1c4e7105aae04cf82c7859fa062
Iain Duff <iain_duff@hotmail.co.uk>
2014-02-17 21:53:51 +0000

    Refactored the unit tests in CardToBoardTranslatorTest to remove the large method testing valid card number to board grid translations and replace it with several smaller test methods, and also made the total number of cards an instance variable.

--- src/test/java/com/duffmanstudios/memory/CardToBoardTranslatorTest.scala
+++ src/test/java/com/duffmanstudios/memory/CardToBoardTranslatorTest.scala
@@ -7,54 +7,72 @@ package com.duffmanstudios.memory
  */
 class CardToBoardTranslatorTest extends ProjectTest {
 
-  "translateCardNumberIntoBoardGridLocation" must "return the correct location in the board depending on the card number selected" in {
-    val first = 1
-    val second = 2
-    val third = 3
-    val fourth = 4
-    val last = game.board.getNumCards
-    val totalNumCards = game.board.getNumCards
+  val totalNumberOfCards = game.board.getNumCards
 
-    val translatedFirst = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(first, totalNumCards)
-    val translatedSecond = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(second, totalNumCards)
-    val translatedThird = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(third, totalNumCards)
-    val translatedFourth = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(fourth, totalNumCards)
-    val translatedLast = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(last, totalNumCards)
+  "translateCardNumberIntoBoardGridLocation" must "return the cell in the first column of the first row for the very first card" in {
+    val numberOfFirstCard = 1
 
-    translatedFirst should equal ((0, 0))
-    translatedSecond should equal ((1, 0))
-    translatedThird should equal ((2, 0))
-    translatedFourth should equal ((3, 0))
-    translatedLast should equal ((1, 2))
+    val translated = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(numberOfFirstCard, totalNumberOfCards)
+
+    translated should equal ((0, 0))
+  }
+
+  "translateCardNumberIntoBoardGridLocation" must "return the cell in the second column of the first row for the second card" in {
+    val numberOfSecondCard = 2
+
+    val translated = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(numberOfSecondCard, totalNumberOfCards)
+
+    translated should equal ((1, 0))
+  }
+
+  "translateCardNumberIntoBoardGridLocation" must "return the cell in the third column of the first row for the third card" in {
+    val numberOfThirdCard = 3
+
+    val translated = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(numberOfThirdCard, totalNumberOfCards)
+
+    translated should equal ((2, 0))
+  }
+
+  "translateCardNumberIntoBoardGridLocation" must "return the cell in the fourth column of the first row for the fourth card" in {
+    val numberOfFourthCard = 4
+
+    val translated = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(numberOfFourthCard, totalNumberOfCards)
+
+    translated should equal ((3, 0))
+  }
+
+  "translateCardNumberIntoBoardGridLocation" must "return the tenth location on the board (working left to right then down a row) for the tenth card in the game" in {
+    val numberOfTenthCard = 10
+
+    val translated = CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(numberOfTenthCard, totalNumberOfCards)
+
+    translated should equal((1, 2))
   }
 
     "checkNumberValid" must "throw an IllegalArgumentException when the card number is below zero" in {
-      val totalNumOfCards = game.board.getNumCards
       val illegalNumber = 0
       intercept[IllegalArgumentException] {
-        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(illegalNumber, totalNumOfCards)
+        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(illegalNumber, totalNumberOfCards)
       }
     }
 
     "checkNumberValid" must "throw an IllegalArgumentException when the card number is greater than the total number of cards" in {
-      val totalNumOfCards = game.board.getNumCards
-      val illegalNumber = totalNumOfCards + 1
+      val illegalNumber = totalNumberOfCards + 1
 
       intercept[IllegalArgumentException] {
-        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(illegalNumber, totalNumOfCards)
+        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(illegalNumber, totalNumberOfCards)
       }
     }
 
     "checkNumberValid" must "NOT throw an Exception if the card number is between 1 and the total number of cards inclusive" in {
       val validNumberOne = 1
       val validNumberTwo = 5
-      val validNumberThree = game.board.getNumCards
-      val totalCardsInGame = game.board.getNumCards
+      val validNumberThree = totalNumberOfCards
 
       try {
-        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(validNumberOne, totalCardsInGame)
-        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(validNumberTwo, totalCardsInGame)
-        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(validNumberThree, totalCardsInGame)
+        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(validNumberOne, totalNumberOfCards)
+        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(validNumberTwo, totalNumberOfCards)
+        CardToBoardTranslator.translateCardNumberIntoBoardGridLocation(validNumberThree, totalNumberOfCards)
       } catch {
         case e: IllegalArgumentException => fail("Exceptions thrown for valid numbers")
       }
